421d8b61c47d855b21663d4d76e2558a2347e103
James Bennett <james@b-list.org>
2011-12-06 16:06:05 -0500

    Do the matching right.

--- soapbox/models.py
+++ soapbox/models.py
@@ -8,16 +8,16 @@ class MessageManager(models.Manager):
     def match(self, url):
         results = []
         for message in self.active():
-            if message.match(url):
+            if message.is_global or message.match(url):
                 results.append(message)
-        return results
+        return list(set(results))
 
 
 class Message(models.Model):
     message = models.TextField()
     is_global = models.BooleanField(help_text="If checked, this message will display on all pages.")
     is_active = models.BooleanField(default=True, help_text="Only active messages will be displayed.")
-    url = models.TextField("URL", max_length=255, blank=True, null=True,
+    url = models.CharField("URL", max_length=255, blank=True, null=True,
                            help_text="Message will be displayed on any URL which matches this.")
 
     objects = MessageManager()
@@ -25,5 +25,11 @@ class Message(models.Model):
     def __unicode__(self):
         return self.message
 
-    def match(url):
-        return url.find(self.url) == 0
+    def match(self, url):
+        if self.is_global:
+            return True
+        self_bits = self.url.strip('/').split('/')
+        url_bits = url.strip('/').split('/')
+        if len(self_bits) > len(url_bits):
+            return False
+        return self_bits == url_bits[:len(self_bits)]
