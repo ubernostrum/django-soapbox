b5731abf656cb48ff3f7d52511b0f0c2ed122514
James Bennett <james@b-list.org>
2015-04-21 20:43:06 -0500

    Add migrations.

---
+++ soapbox/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Message',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('message', models.TextField()),
+                ('is_global', models.BooleanField(help_text=b'If checked, this message will display on all pages.')),
+                ('is_active', models.BooleanField(default=True, help_text=b'Only active messages will be displayed.')),
+                ('url', models.CharField(help_text=b'Message will be displayed on any URL which matches this.', max_length=255, null=True, verbose_name=b'URL', blank=True)),
+            ],
+            options={
+                'ordering': ['-id'],
+            },
+        ),
+    ]
---
+++ soapbox/migrations/0002_is_global_default.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('soapbox', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='message',
+            name='is_global',
+            field=models.BooleanField(default=False, help_text=b'If checked, this message will display on all pages.'),
+        ),
+    ]
