5b8fc58abb9d9d4f48fe7cce85a391f7ebecb791
James Bennett <james@b-list.org>
2015-04-22 20:28:31 -0500

    Some final packaging tweaks.

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 setup(name='django-soapbox',
       version='1.1',
+      zip_safe=False, # eggs are the devil.
       description='Site-wide and page-specific announcements/messages for Django sites',
       long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
       author='James Bennett',
       author_email='james@b-list.org',
       url='https://github.com/ubernostrum/django-soapbox',
-      download_url='http://static.b-list.org//files/django-soapbox-1.0.tar.gz', 
       packages=['soapbox', 'soapbox.templatetags', 'soapbox.tests', 'soapbox.templates'],
       package_dir={'soapbox': 'soapbox'},
       package_data={'soapbox': ['templates/soapboxtest/*.html']},
