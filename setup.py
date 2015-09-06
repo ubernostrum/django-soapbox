138f62dd7212dc77640e89392aba1b5891f5ac18
James Bennett <james@b-list.org>
2015-09-05 20:42:34 -0700

    Get ready to release 1.2.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 setup(name='django-soapbox',
-      version='1.1',
+      version='1.2',
       zip_safe=False,  # eggs are the devil.
       description=("Site-wide and page-specific announcements/messages for "
                    "Django sites"),
